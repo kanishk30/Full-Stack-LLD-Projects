2a78078bddf063927437f49a28f328b8f85df925
kanishk30 <kanishk71@gmail.com>
2024-05-21 08:31:05 +0530

    var, let and const

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,28 @@
-console.log('I am running from external JS file.')
+console.log('I am running from external JS file.');
+
+// valid JS
+var a = true;
+console.log(a);
+
+var a = false;
+console.log(a);
+
+// will throw error
+// let city = 'Bangalore';
+// console.log(city);
+
+// let city = 'Delhi';
+// console.log(city);
+
+
+// will work
+// city = 'Blr';
+// console.log(city);
+
+
+// throw error.
+// const goat = 'MSDhoni';
+// goat = 'Kohli';
+
+const pi = 3.14;
+pi = 3.15
