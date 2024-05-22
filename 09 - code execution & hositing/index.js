d2978253e9b8e7176861a04b734d4983a9a5dc28
kanishk30 <kanishk71@gmail.com>
2024-05-22 10:10:48 +0530

    hoisting

--- 09 - code execution & hositing/index.js
+++ 09 - code execution & hositing/index.js
@@ -19,13 +19,47 @@
 
 // hoisting.
 
-console.log(msg);
-var msg = 'hello';
+// console.log(msg);
+// var msg = 'hello';
+// // greet();
+// console.log(greet());
+// function greet() {
+//     console.log(msg, ' inside greet');
+// }
+
+
+
+// let someVal = 'hello world';
+// var msg = 'hello';
+
+// function greet() {
+//     var msg = 'i m msg inside greet.'
+//     console.log(msg, ' Happy holidays')
+// }
+
+// console.log(msg);
 // greet();
-console.log(greet());
-function greet() {
-    console.log(msg, ' inside greet');
-}
 
 
 
+// var msg = 'hello';
+
+// greet1();
+// greet2();
+
+// console.log(msg);
+// function greet1() {
+//     var msg = 'inside greet1';
+//     console.log(msg);
+// }
+
+// function greet2() {
+//     var msg = 'inside greet2';
+//     console.log(msg);
+// }
+
+
+debugger
+console.log(value);
+// var value = 10; -- will work
+// let value = 20 -- error ( TDZ )
