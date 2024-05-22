d1ab93df4767b72f357fba06e09077c409ac7388
kanishk30 <kanishk71@gmail.com>
2024-05-22 08:47:02 +0530

    hositing ex.

--- 09 - code execution & hositing/index.js
+++ 09 - code execution & hositing/index.js
@@ -1,18 +1,31 @@
-// function declaration
+// // function declaration
 
-function sayHi() {
-    console.log('Hi')
-};
+// function sayHi() {
+//     console.log('Hi')
+// };
 
-// sayHi();
+// // sayHi();
 
-// functions as expressions
+// // functions as expressions
 
-let sayHiFromExpression = function() {
-    console.log('Hi, I am a first class citizen');
+// let sayHiFromExpression = function() {
+//     console.log('Hi, I am a first class citizen');
     
-};
+// };
 
-sayHiFromExpression();
-console.log(sayHiFromExpression())
+// sayHiFromExpression();
+// console.log(sayHiFromExpression())
+
+
+// hoisting.
+
+console.log(msg);
+var msg = 'hello';
+// greet();
+console.log(greet());
+function greet() {
+    console.log(msg, ' inside greet');
+}
+
+
 
