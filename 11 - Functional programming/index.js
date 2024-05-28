db27d8cca929980ab1bd1f4858a7af37fda60281
kanishk30 <kanishk71@gmail.com>
2024-05-28 08:16:58 +0530

    pure & impure functions

--- 11 - Functional programming/index.js
+++ 11 - Functional programming/index.js
@@ -1,20 +1,43 @@
-function printName(cb1, cb2) {
-    // cb1 = printLastName
-    // cb2 = printJob
-    console.log('Kanishk');
-    cb1(cb2);
+// function printName(cb1, cb2) {
+//     // cb1 = printLastName
+//     // cb2 = printJob
+//     console.log('Kanishk');
+//     cb1(cb2);
     
+// }
+
+// function printLastName(cb) {
+//     // cb = printJob
+//     cb()
+//     console.log('Agrawal');
+// };
+
+// function printJob() {
+//     console.log('Job - Engineer')
+// }
+
+// // printName(printLastName)
+// printName(printLastName, printJob); // Kanishk Job - Engineer  Agrawal
+
+
+function sum(a,b) {
+    return a + b;
 }
 
-function printLastName(cb) {
-    // cb = printJob
-    cb()
-    console.log('Agrawal');
-};
+console.log(sum(1,2));
+console.log(sum(1,2));
+console.log(sum(1,2));
+console.log(sum(1,2));
+console.log(sum(1,2));
+console.log(sum(1,2));
+console.log(sum(1,2));
+console.log(sum(1,2));
 
-function printJob() {
-    console.log('Job - Engineer')
+
+var c = 0;
+
+function impureSum(a, b) {
+    return a + b + c++;
 }
-
-// printName(printLastName)
-printName(printLastName, printJob); // Kanishk Job - Engineer  Agrawal
+console.log('Impure sum - ', impureSum(2,3))
+console.log('Impure sum - ', impureSum(2,3))
