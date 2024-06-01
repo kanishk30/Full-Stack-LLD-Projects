b888532b60383428edbfcef236ced0abe7e8db97
kanishk30 <kanishk71@gmail.com>
2024-06-01 08:16:43 +0530

    Promises

---
+++ 13 - async - promises/index.js
@@ -0,0 +1,106 @@
+
+// const fs = require('fs');
+
+// fs.readFile('f1.txt', function(err, data) {
+//     if(err) {
+//         console.log('Error');
+//         return;
+//     } else {
+//         const processedData = data.toString();
+//         console.log(processedData);
+        
+//         fs.writeFile('f2.txt', processedData, function(error, data2) {
+//             if(error) {
+//                 console.log(error);
+//                 return;
+//             } else {
+//                 // fs.writeFile('f2.txt', processedData, function(error, data2) {
+//                 //     if(error) {
+//                 //         console.log(error);
+//                 //         return;
+//                 //     } else {
+//                 //         fs.writeFile('f3.txt', data, func)
+//                 //     }
+                    
+        
+//                 // });
+//             }
+            
+
+//         });
+
+//     };
+
+
+
+
+// });
+
+
+
+
+
+// user1 > all posts (p1,p2,p3) of user1 
+// p2 > all comments > c1, c2, c3....
+
+// user1 >> p2 >> c1
+
+
+// function fetchUser(userId) {
+//     function fetchUserData(userId, (err, userData)) {
+//       if (err) handleError("fetching user data", err);
+//       else {
+//         log("User data fetched", userData);
+//         function fetchUserPosts(userData.id, function (err, posts)) {
+
+//           else {
+//             log("Posts fetched", posts);
+//             posts.forEach((post) => {
+//               function fetchCommentsByPost(post.id, function (err, comments)  {
+//                 if (err) handleError("fetching comments for post", err, post.id);
+//                 else log("Comments for post", post.id, comments);
+//                 // Potential for more nested callbacks here
+//               });
+//             };
+//           }
+//         };
+//       }
+//     });
+//    }
+
+
+// promise
+
+
+// 1. CREATION OF PROMISE
+let myPromise = new Promise(function(resolve, reject) {
+// resolve - function when task is successful
+// reject - function when task is failed(rejected)
+
+    setTimeout(() => {
+        const isHeads = Math.random() > 0.5;
+        if(isHeads) {
+            resolve('I win. It is Heads')
+        } else {
+            reject('Lost. Tails')
+        }
+
+    }, 500)
+})
+
+// 2. CONSUMING THE PROMISE
+// .then() .catch() .finally() [less used]
+
+myPromise.then(function(result) {
+    console.log('Success (then):', result);
+})
+.catch(function (err) {
+    console.log('Error(catch):', err)
+})
+.finally(function() {
+    console.log('I will always be called')
+})
+   
+
+
+// CHAINING
