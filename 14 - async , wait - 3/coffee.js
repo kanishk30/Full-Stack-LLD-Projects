9d351a54927e8636889c581269375582a89b6c73
kanishk30 <kanishk71@gmail.com>
2024-06-04 10:35:50 +0530

    coffee async await

--- 14 - async , wait - 3/coffee.js
+++ 14 - async , wait - 3/coffee.js
@@ -1,14 +1,22 @@
 // step1 = create a promise  for placing / accept the order.
 
 
-function placeOrder(drink) {
-    return new Promise(function(resolve, reject) {
+// function placeOrder(drink) {
+//     return new Promise(function(resolve, reject) {
+//         if(drink ==='coffee') {
+//             resolve('Order placed for one coffee')
+//         } else {
+//             reject('Order cannot be placed.')
+//         }
+//     })
+// }
+
+async function placeOrder(drink) {
         if(drink ==='coffee') {
-            resolve('Order placed for one coffee')
+            return 'Order placed for one coffee'
         } else {
-            reject('Order cannot be placed.')
+            return 'Order cannot be placed.'
         }
-    })
 }
 
 // step 2 - create a promise to process the order.
@@ -25,32 +33,53 @@ function generateBill(processedOrder) {
     })
 }
 
-placeOrder('coffee')
-.then(function(orderStatus) {
-    console.log(orderStatus, '1');
-    return orderStatus;
-})
-.then(function(orderStatussss) {
-    let orderProcessedPromise = processOrder(orderStatussss);
-    console.log(orderProcessedPromise, '2');
-    return orderProcessedPromise
-})
-.then(function(isOrderProcessed) {
-    console.log(isOrderProcessed, '3');
-    return isOrderProcessed
-})
-.then(function(orderProcess) {
-    let isBillGeneratedPromise = generateBill(orderProcess);
-    return isBillGeneratedPromise
-})
-.then(function(billResolvedValue) {
-    console.log(billResolvedValue, '4')
-})
-.catch(function(err) {
-    console.log(err)
-})
+// placeOrder('coffee')
+// .then(function(orderStatus) {
+//     console.log(orderStatus, '1');
+//     return orderStatus;
+// })
+// .then(function(orderStatussss) {
+//     let orderProcessedPromise = processOrder(orderStatussss);
+//     console.log(orderProcessedPromise, '2');
+//     return orderProcessedPromise
+// })
+// .then(function(isOrderProcessed) {
+//     console.log(isOrderProcessed, '3');
+//     return isOrderProcessed
+// })
+// .then(function(orderProcess) {
+//     let isBillGeneratedPromise = generateBill(orderProcess);
+//     return isBillGeneratedPromise
+// })
+// .then(function(billResolvedValue) {
+//     console.log(billResolvedValue, '4')
+// })
+// .catch(function(err) {
+//     console.log(err)
+// })
 
 
 // step 3 - generate a bill if coffee is served
 
+
+// using async await.
+
+
+async function serveOrder() {
+
+    try {
+        // BLOCKED until placeorder retyrn the value
+        let orderStatus = await placeOrder('coffee');
+        console.log(orderStatus, '1');
+
+        let processedOrder = await processOrder(orderStatus);
+        console.log(processedOrder, '2')
+
+        let generatedBill = await generateBill(processedOrder);
+        console.log(generatedBill);
+    } catch(error) {
+        console.log(error, 'i am inside catch');
+    }
+}
+serveOrder()
 
