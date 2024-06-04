144b82fc5cea3978871441c631452402e84db043
kanishk30 <kanishk71@gmail.com>
2024-06-04 08:42:11 +0530

    async await chaining

--- 14 - async , wait - 3/index.js
+++ 14 - async , wait - 3/index.js
@@ -26,6 +26,12 @@ const p = new Promise(function(resolve, reject) {
     }, 2000)
 });
 
+const p2 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('Promise resolved2');
+    }, 1000)
+});
+
 async function fetchData() {
     // p.then(function(result) {
     //     console.log(result, '[From .then]');
@@ -44,8 +50,75 @@ async function fetchData() {
 //     console.log(val, '[From async/await]');
 // }
 
+async function handlePromise2() {
+    console.log('Life:')
+    const val = await p; // this will return 'Promise resolved'
+    console.log('Create impact 1');
+    console.log(val);
+
+    const val2 = await p; // // this will return 'Promise resolved'
+    console.log('Create impact 2');
+    console.log(val2);
+
+// Life:
+// 8 seconds pause only here.
+// Create impact 1
+// Promise resolved
+// Create impact 2
+// Promise resolved
+}
+
+async function handlePromise3() {
+    console.log(new Date(), 'start')
+    console.log('Life:');
+    const val = await p; //  8s
+    console.log(new Date(), 'p1')
+    console.log('Create impact 1');
+    console.log('p1: ', val);
+
+    const val2 = await p2; // 3s
+    console.log(new Date(), 'p2')
+    console.log('Create impact 2');
+    console.log('p2: ', val2);
+
+// Life:
+// 8 seconds pause only here.
+// Create impact 1
+// Promise resolved
+// Create impact 2
+// Promise resolved
+}
 
 
-fetchData();
+
+// fetchData();
 // handlePromise()
+// handlePromise2()
+// handlePromise3()
+
+
+function test1() {
+    console.log('test')
+};
+
+const test2 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        resolve('test2')
+    }, 3000)
+})
+
+function test3() {
+    console.log('test3')
+}
+
+async function getData() {
+    console.log('4');
+    const result2 = await test2;
+    console.log(result2);
+    
+}
+
+test1();
+getData();
+test3();
 
