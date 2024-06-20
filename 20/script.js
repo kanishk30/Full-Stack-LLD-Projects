cd4d70391492d67cc5b65ae7263c51e779be1d10
kanishk30 <kanishk71@gmail.com>
2024-06-20 08:03:01 +0530

    delete & locking of tickets

---
+++ 20/script.js
@@ -0,0 +1,150 @@
+const addBtn = document.querySelector('.add-btn');
+const modalCont = document.querySelector('.modal-cont');
+const mainCont = document.querySelector('.main-cont');
+const textArea = document.querySelector('.text-area-cont');
+const allPriorityColors = document.querySelectorAll('.priority-color');
+const removeBtn = document.querySelector('.remove-btn');
+
+let modalPriorityColor = 'black';
+let addTaskFlag = false;
+let removeTaskFlag = false;
+
+const lockClose = 'fa-lock';
+const lockOpen = 'fa-lock-open';
+
+
+addBtn.addEventListener('click', function(e) {
+    addTaskFlag = !addTaskFlag;
+    // if(addTaskFlag) {
+    //     addTaskFlag = false;
+    // } else {
+    //     addTaskFlag = true;
+    // }
+
+    if(addTaskFlag) {
+        modalCont.style.display = 'flex'
+    } else {
+        modalCont.style.display = 'none'
+    }
+})
+
+removeBtn.addEventListener('click', function() {
+    removeTaskFlag = !removeTaskFlag; // toggle the remve flag when btn is clicked.
+
+    if(removeTaskFlag) {
+        alert('Delete button activated.'); // present on window object.
+        removeBtn.style.color = 'red';
+
+    } else {
+        removeBtn.style.color = 'white'
+    }
+
+})
+
+function handleRemoval(ticketElem) {
+    const allTickets = document.querySelectorAll('.ticket-cont');
+
+    ticketElem.addEventListener('click', function() {
+        console.log(ticketElem, '[handleRemoval]')
+        if(removeTaskFlag) {
+            ticketElem.remove();
+        }
+    })
+}
+const allTickets = document.querySelectorAll('.ticket-cont');
+
+allTickets.forEach(function(ticketNode) {
+    console.log('all ticket event attach')
+    handleRemoval(ticketNode)
+})
+
+function createTicket(modalPriorityColor, textValue, ticketID) {
+    const ticketCont = document.createElement('div');
+    // ticketCont.classList.add('ticket-cont');
+    // // OR
+    ticketCont.setAttribute('class', 'ticket-cont');
+
+    ticketCont.innerHTML = `
+    <div class="ticket-color" style='background-color: ${modalPriorityColor}'></div>
+    <div class="ticket-id">${ticketID}</div>
+    <div class="task-area">${textValue}</div>
+    <div class="ticket-lock">
+        <i class="fa-solid fa-lock"></i>
+    </div>
+    `
+
+    mainCont.appendChild(ticketCont);
+    handleLock(ticketCont);
+    handleRemoval(ticketCont);
+
+}
+
+modalCont.addEventListener('keydown', function(e) {
+    const key = e.key;
+    console.log(textArea.value)
+    if(key === 'Shift') {
+
+        // const tktId = shortid();
+
+        const tktId = Math.random().toString(36).substring(2);
+        console.log(tktId, '[ticketID]')
+
+        createTicket(modalPriorityColor, textArea.value, tktId);
+        modalCont.style.display = 'none';
+        textArea.value = ''
+
+    } 
+})
+
+
+// color active class apply.
+allPriorityColors.forEach(function(colorElem, index) {
+    colorElem.addEventListener('click', function() {
+        // removed active class
+        allPriorityColors.forEach(function (priorityColorElem) {
+            priorityColorElem.classList.remove('active')
+        })
+        
+        // add active class on selected element
+        colorElem.classList.add('active');
+        console.log(colorElem.classList);
+        console.log(colorElem.classList[0]);
+        modalPriorityColor = colorElem.classList[0]; // selected color.
+        
+    })
+})
+
+function handleLock(ticket) {
+    const ticketNode = ticket.querySelector('.ticket-lock');
+    const ticketLockIcon = ticketNode.children[0]; // <i>...</i>
+    const ticketTaskArea = ticket.querySelector('.task-area');
+
+    ticketLockIcon.addEventListener('click', function() {
+        console.log('lock selected');
+
+        if(ticketLockIcon.classList.contains(lockClose)) {
+            // add lockopen class
+            // remove lockclose class
+            console.log('lock open')
+            ticketLockIcon.classList.add(lockOpen);
+            ticketLockIcon.classList.remove(lockClose);
+
+            ticketTaskArea.setAttribute('contenteditable', 'true')
+
+
+        } else {
+            console.log('lock closed')
+            ticketLockIcon.classList.remove(lockOpen);
+            ticketLockIcon.classList.add(lockClose);
+            ticketTaskArea.setAttribute('contenteditable', 'false')
+        }
+    })
+}
+
+
+
+
+
+
+
+// createTicket();
