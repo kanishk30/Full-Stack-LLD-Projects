2e362e06a4b911d9a9d827559f240b379eecfa08
kanishk30 <kanishk71@gmail.com>
2024-06-22 08:05:23 +0530

    capture variations

--- 21/script.js
+++ 21/script.js
@@ -4,24 +4,29 @@ const child = document.querySelector('#child');
 
 // bubbling
 // grandparent.addEventListener('click', function() {
-//     console.log('grandparent clicked')
+//     console.log('grandparent clicked (Bubbling)')
 // }, false)
 
 // parent.addEventListener('click', function() {
-//     console.log('parent clicked')
+//     console.log('parent clicked (Bubbling)')
 // })
 
-// child.addEventListener('click', function() {
-//     console.log('child clicked')
+// child.addEventListener('click', function(e) {
+//     console.log('child clicked (Bubbling)');
+    
+//     // stop propagation after clicking the child element.
+//     e.stopPropagation();
 // })
 
 // event capturing
+// useCapture paramerter ( third param (boolean))
 grandparent.addEventListener('click', function() {
     console.log('grandparent clicked capturing')
 }, true)
 
-parent.addEventListener('click', function() {
+parent.addEventListener('click', function(e) {
     console.log('parent clicked capturing')
+    e.stopPropagation();
 }, true)
 
 child.addEventListener('click', function() {
