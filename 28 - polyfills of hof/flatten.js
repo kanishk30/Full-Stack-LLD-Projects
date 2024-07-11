16bee5f1f81d4b5fc29d169acd27de4a3719672e
kanishk30 <kanishk71@gmail.com>
2024-07-11 09:29:25 +0530

    questions

---
+++ 28 - polyfills of hof/flatten.js
@@ -0,0 +1,17 @@
+// Q. WAF in JS to flatten a nested array into a single dimension array.
+// where all nested elements move towards the top of the array.
+
+const nestedArray = [1, [2, 3], [4, [5, 6]]];
+
+// flatten array 
+// output  >> [1, 2, 3, 4, 5, 6]
+
+
+function flattenArray(arr) {
+
+    // implement
+
+    // return an array in flatten form
+
+
+}
