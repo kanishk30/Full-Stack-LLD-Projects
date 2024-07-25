9918fdb9f9b10e4136f1614d009251606a7d155a
kanishk30 <kanishk71@gmail.com>
2024-07-25 08:12:08 +0530

    Vite setup for React

---
+++ 32 - build tools, props/myfirst-app/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyComponent from './MyComponent'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hello, I am a component........</h1>
+      <MyComponent /> {/* first use */}
+      <MyComponent /> {/* 2nd use */}
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+
+  
+    </>
+  )
+}
+
+export default App
---
+++ 32 - build tools, props/myfirst-app/src/MyComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return <h1>Hello, I am a component.</h1>
+}
+
+export default MyComponent;
