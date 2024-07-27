c5ff896d2e9af26064cc7c5067d4365e7ebefb34
kanishk30 <kanishk71@gmail.com>
2024-07-27 09:08:21 +0530

    useeffect

--- 32 - build tools, props/myfirst-app/src/App.jsx
+++ 32 - build tools, props/myfirst-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -13,23 +13,53 @@ import TemperatureDisplay from './TemperatureDisplay'
 
 function App() {
 
-  const [temperature, setTemperature] = useState('');
+  // const [temperature, setTemperature] = useState('');
 
-  const handleTemperatureChange = (newTemp) => {
-    setTemperature(newTemp);
-  }
+  // const handleTemperatureChange = (newTemp) => {
+  //   setTemperature(newTemp);
+  // }
 
-  return (
-    <>
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('HI', count);
+    document.title = `You clicked ${count} times.`
+  }, [count]) // dependency
+  // [] > only run on mount. no dependency to change.
+
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+        console.log('Timer ticked.')
+    }, 1000)
+
+    // cleanup (function) this ( unmount )
+    return () => {
+      clearInterval(timer)
+    }
+
+  }, [])
+
+
+  return <div>
+    <p>You clicked: {count} times.</p>
+    <button onClick={() => setCount(count+1)}>
+        Click me to add 1
+    </button>
+  </div>
+
+
+  // return (
+    // <>
     {/* <SimpleForm/> */}
     {/* <AdvancedForm/> */}
-    <TemperatureInput 
+    {/* <TemperatureInput 
       temperature={temperature} 
       handleTemperatureChange={handleTemperatureChange}
     />
     <TemperatureDisplay temperature={temperature} />
-    </>
-  )
+    </> */}
+  // )
 }
 
 export default App
