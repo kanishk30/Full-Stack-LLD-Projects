589e0b8ac8eae702ecebff1aafd4de55c3207108
kanishk30 <kanishk71@gmail.com>
2024-08-06 08:46:32 +0530

    Search movies

--- movies-app/src/Components/WatchList.jsx
+++ movies-app/src/Components/WatchList.jsx
@@ -9,6 +9,9 @@ const getGenreName = genre_id => {
 
 function WatchList() {
   const [watchList, setWatchList] = useState([]);
+
+  const [searchedStr, setSearch] = useState('')
+
   useEffect(() => {
     let moviesFromLS = localStorage.getItem('movies');
     if (moviesFromLS) {
@@ -34,47 +37,62 @@ function WatchList() {
 
 
   return (
-    <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
-      <table className="w-full border-collapse bg-white text-left text-sm
-  text-gray-500">
-        <thead>
-          <tr className="bg-gray-50">
-            <th className="px-6 py-4 font-medium text-gray-900">Name</th>
-            <th>
-              <div className="flex">
-                <i className='fa-solid fa-arrow-up cursor-pointer pr-1' onClick={handleAscRatings}></i>
-                <div>Ratings</div>
-                <i className='fa-solid fa-arrow-down cursor-pointer pl-1' onClick={handleDescRatings}></i>
-              </div>
-            </th>
-            <th>
-              <div className="flex">
-                <div>Popularity</div>
-              </div>
-            </th>
-            <th>
-              <div className="flex">
-                <div>Genre</div>
-              </div>
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-          {watchList.map((movie) => {
-            return (<tr className="hover:bg-gray-50">
-              <td className="flex items-center px-6 py-4 font-normal text-gray-900">
-                <img className="h-[6rem] w-[10rem] object-fit" src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title} />
-                <div className="font-medium text-gray-700 text-sm">{movie.title}</div>
-              </td>
-              <td className="pl-6 py-4">{movie.vote_average}</td>
-              <td className="pl-6 py-4">{movie.popularity}</td>
-              <td className="pl-2 py-4">{getGenreName(movie.genre_ids[0])}</td>
-            </tr>)
-          })}
+    <>
+      {/* Search field:: */}
+      <div className='flex justify-center my-5'>
+        <input
+          placeholder='search movies'
+          type='text'
+          value={searchedStr}
+          onChange={(e) => setSearch(e.target.value)}
+          className='h-8 w-[300px] bg-gray-200 px-4 outline-none border border-slate-700'
+        />
+      </div>
 
-        </tbody>
-      </table>
-    </div>
+      <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+        <table className="w-full border-collapse bg-white text-left text-sm
+  text-gray-500">
+          <thead>
+            <tr className="bg-gray-50">
+              <th className="px-6 py-4 font-medium text-gray-900">Name</th>
+              <th>
+                <div className="flex">
+                  <i className='fa-solid fa-arrow-up cursor-pointer pr-1' onClick={handleAscRatings}></i>
+                  <div>Ratings</div>
+                  <i className='fa-solid fa-arrow-down cursor-pointer pl-1' onClick={handleDescRatings}></i>
+                </div>
+              </th>
+              <th>
+                <div className="flex">
+                  <div>Popularity</div>
+                </div>
+              </th>
+              <th>
+                <div className="flex">
+                  <div>Genre</div>
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+            {watchList
+              .filter((movie) => movie.title.toLowerCase().includes(searchedStr.toLowerCase()))
+              .map((movie) => {
+                return (<tr className="hover:bg-gray-50">
+                  <td className="flex items-center px-6 py-4 font-normal text-gray-900">
+                    <img className="h-[6rem] w-[10rem] object-fit" src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title} />
+                    <div className="font-medium text-gray-700 text-sm">{movie.title}</div>
+                  </td>
+                  <td className="pl-6 py-4">{movie.vote_average}</td>
+                  <td className="pl-6 py-4">{movie.popularity}</td>
+                  <td className="pl-2 py-4">{getGenreName(movie.genre_ids[0])}</td>
+                </tr>)
+              })}
+
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 export default WatchList;
